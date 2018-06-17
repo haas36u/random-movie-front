8f4725f202258b06ddd9e8f386af2e6d473ae932
Myriam <haasmyriam8@gmail.com>
2018-06-18 00:39:35 +0200

    Create simple header style. Profile section and menu need to be worked

--- src/components/Base/Header.js
+++ src/components/Base/Header.js
@@ -1,32 +1,31 @@
 import React, {Component} from "react";
 import logo from '../../images/logo.png';
+import { FontIcon, TextField } from 'react-md';
 
 export default class Footer extends Component{
     render() {
         return (
-            <header className="App-header navbar-fixed">
+            <header>
+                <TextField id="search" placeholder="Rechercher" className="search" />
                 <nav>
-                    <div className="nav-wrapper">
-                        <ul className="hide-on-med-and-down brand_nav_right">
-                            <li><a href="">Accueil</a></li>
-                        </ul>
-                        <ul className="hide-on-med-and-down brand_nav_left">
-                            <li><a href="">Films</a></li>
-                        </ul>
+                    <ul className="nav_right">
+                        <li><a href="">Séries</a></li>
+                    </ul>
+                    <ul className="nav_left">
+                        <li><a href="">Films</a></li>
+                    </ul>
 
-                        <a href="" className="brand-logo center tooltipped" data-position="bottom" data-tooltip="Film aléatoire">
-                            <img src={logo} alt="Logo Random Movie"/>
-                        </a>
+                    <a href="" className="logo center tooltipped" data-position="bottom" data-tooltip="Film aléatoire">
+                        <img src={logo} alt="Logo Random Movie"/>
+                    </a>
 
-                        <ul className="right hide-on-med-and-down">
-                            <li>
-                                <a href={this.login}>
-                                    <i className="material-icons hide-on-large-only">account_circle</i>
-                                    <span className="hide-on-med-and-down">Connexion</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    <ul className="header_profile">
+                        <li>
+                            <a href={this.login}>
+                                <span className="hide-on-med-and-down">Connexion</span>
+                            </a>
+                        </li>
+                    </ul>
                 </nav>
             </header>
         )
