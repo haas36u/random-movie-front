933c0fa19ef5db34a19da14869152c9398462d0b
Myriam <haasmyriam8@gmail.com>
2018-06-22 00:29:39 +0200

    Create movie show page with movie informations

---
+++ src/containers/Movie/MovieShow.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Grid, Cell, Button, FontIcon } from 'react-md';
+
+import poster from '../../images/deadpool.jpg';
+import actor from '../../images/ryan_reynolds.jpg';
+import popular1 from '../../images/starwars.jpg';
+
+export default class MovieShow extends Component {
+  
+  render() {
+    
+    return (
+      <div id="movieShow">
+        
+        <div id="movieShow_informations-container">
+            <div class="container">
+                <Grid>
+                    <Cell size={4}><img src={poster} alt="Poster deadpool"/></Cell>
+                    <Cell size={8}>
+                        <Grid>
+                            <Cell size={6} id="movieShow_informations-container_infos">
+                                <h1>Deadpool</h1>
+                                <p><span className="text-bold">Date de sortie :</span>22/12/2018</p>
+                                <p><span className="text-bold">Durée :</span>120 min</p>
+                                <p><span className="text-bold">Genres :</span> Aventure | Humour</p>
+                                <p><span className="text-bold">Langue originale :</span> Anglais</p>
+                                <p className="public_rate">Spectateurs</p>
+                                <div>
+                                    <span>Aucune note pour ce film</span>
+                                </div>
+                            </Cell>
+                            <Cell size={6}>
+                                <a href="{{ path('movie_random') }}" className="btn"> <i className="fas fa-random"></i>Film aléatoire</a>
+                                <Button icon className="addToWishList" active data-movie_id="1" tooltipLabel="Ajouter à vos favoris"><i className="fas fa-heart"></i></Button>
+                                <Button icon className="addToWishList" active data-movie_id="1" tooltipLabel="Ajouter aux films déjà vus"><i className="fas fa-eye"></i></Button>
+                                <Button icon className="addToWishList" active data-movie_id="1" tooltipLabel="Ajouter à votre liste de film à voir"><i className="fas fa-add"></i></Button>
+                            </Cell>
+                            <Cell size={12}>
+                                <h5>Synopsis et détails</h5>
+                                <p>Le super descriptif du film</p>
+                                <Grid>
+                                    <Cell size={6}>
+                                        <div className="text-bold">Votre note</div>
+                                        <div id="movie-rating" data-movie_id="1" data-rate="1"></div>
+                                    </Cell>
+                                    <Cell size={6}>
+                                        <a data-movie_id="1" className="btn" href="">
+                                            <i className="fas fa-edit"></i>Ajouter un commentaire
+                                        </a>
+                                    </Cell>
+                                </Grid>
+                            </Cell>
+                        </Grid>
+                    </Cell>
+                </Grid>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Homepage from '../containers/Homepage';
+import MovieShow from '../containers/Movie/MovieShow';
+
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Base/Header';
 import Footer from '../components/Base/Footer';
@@ -12,6 +14,7 @@ const AppRouter = () => (
         <div className="main">
             <Switch>
                 <Route path="/" component={Homepage} exact={true} />
+                <Route path="/movies/id" component={MovieShow} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
