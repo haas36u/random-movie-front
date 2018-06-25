def125a5d6f7935d5e2f7c02b7e66129a1cd1229
Myriam <haasmyriam8@gmail.com>
2018-06-26 00:14:45 +0200

    Create suggestion section in movieShow page

--- src/containers/Movie/MovieShow.js
+++ src/containers/Movie/MovieShow.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell, Button, FontIcon, Card, CardText } from 'react-md';
+import { Grid, Cell, Button, FontIcon, Card, CardText, Media, MediaOverlay, CardTitle } from 'react-md';
 
 import poster from '../../images/deadpool.jpg';
 import actor from '../../images/ryan_reynolds.jpg';
@@ -10,8 +10,13 @@ export default class MovieShow extends Component {
     render() {
 
         var imgUrl = require('../../images/ryan_reynolds.jpg');
-        var divStyle = {
+        var starWars = require('../../images/deadpool.jpg');
+        var avatarComments = {
             backgroundImage: 'url(' + imgUrl + ')'
+        }
+
+        var starWarsBg = {
+            backgroundImage: 'url(' + starWars + ')'
         }
     
         return (
@@ -128,7 +133,7 @@ export default class MovieShow extends Component {
                             <Cell size={6} className="ml-0">
                                 <Grid className="pl-0">
                                     <Cell size={2}>
-                                    <div className="avatar_comments_container" background style={divStyle}></div>
+                                    <div className="avatar_comments_container" style={avatarComments}></div>
                                     </Cell>
                                     <Cell size={10}>
                                         <p className="title_comments m-0">Commentaire positif le plus utile</p>
@@ -148,7 +153,7 @@ export default class MovieShow extends Component {
                             <Cell size={6} className="ml-0">
                                 <Grid className="pl-0">
                                     <Cell size={3}>
-                                    <div className="avatar_comments_container" background style={divStyle}></div>
+                                    <div className="avatar_comments_container" style={avatarComments}></div>
                                     </Cell>
                                     <Cell size={9}>
                                         <p className="title_comments m-0">Commentaire négatif le plus utile</p>
@@ -166,6 +171,51 @@ export default class MovieShow extends Component {
                                 </Grid>
                             </Cell>
                         </Grid>
+                    </div>
+                </div>
+
+                <div className="container pb-4 pt-4" id="movies-suggestion-container">
+                    <h5 className="pb-1">Nos recommendations</h5>
+                    <div className="movies-suggestion--movies">
+                        <div className="movie_vignette">
+                            <div style={starWarsBg}></div>
+                            <div className="actions_buttons">
+                                <div className="right">
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter à vos favoris"><i className="fas fa-heart"></i></Button>
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter aux films déjà vus"><i className="fas fa-eye"></i></Button>
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter à votre liste de film à voir"><i className="fas fa-plus-square"></i></Button>
+                                </div>
+                            </div>
+                            <div className="movie_title">
+                                <p>Star wars - 22 / 01 / 2018</p>
+                            </div>
+                        </div>
+                        <div className="movie_vignette">
+                            <div style={starWarsBg}></div>
+                            <div className="actions_buttons">
+                                <div className="right">
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter à vos favoris"><i className="fas fa-heart"></i></Button>
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter aux films déjà vus"><i className="fas fa-eye"></i></Button>
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter à votre liste de film à voir"><i className="fas fa-plus-square"></i></Button>
+                                </div>
+                            </div>
+                            <div className="movie_title">
+                                <p>Star wars - 22 / 01 / 2018</p>
+                            </div>
+                        </div>
+                        <div className="movie_vignette">
+                            <div style={starWarsBg}></div>
+                            <div className="actions_buttons">
+                                <div className="right">
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter à vos favoris"><i className="fas fa-heart"></i></Button>
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter aux films déjà vus"><i className="fas fa-eye"></i></Button>
+                                    <Button icon className="addToWishList" active={true} data-movie_id="1" tooltipLabel="Ajouter à votre liste de film à voir"><i className="fas fa-plus-square"></i></Button>
+                                </div>
+                            </div>
+                            <div className="movie_title">
+                                <p>Star wars - 22 / 01 / 2018</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
