f241969389ab34ab82d86a2bfaadc4b699ff89ef
Myriam <haasmyriam8@gmail.com>
2018-06-27 00:33:42 +0200

    Add trianglify and use it as background image in slidenav

--- src/components/Base/Menu.js
+++ src/components/Base/Menu.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import { Button, DialogContainer, NavigationDrawer, SVGIcon } from 'react-md';
 import { Link } from 'react-router-dom';
+var Trianglify = require('trianglify');
+var pattern = Trianglify({width: 200, height: 200})
 
 export default class Menu extends Component {
 
 
   render() {
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
-}
+    const openNav = () => {
+        document.getElementById("sidenav").style.width = "250px";
+    }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-const closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
-}
+    const closeNav = () => {
+        document.getElementById("sidenav").style.width = "0";
+    }
+
+    let pattern = Trianglify({
+        x_colors: 'Blues'
+    });
+
+    let triangle = pattern.png();
+
+    let bgTriangle = {
+        backgroundImage: 'url(' + triangle + ')'
+    }
 
     return (
         <span>
-            <div id="mySidenav" class="sidenav">
-                <span class="closebtn" onClick={closeNav}>&times;</span>
+
+        <div className="hello"></div>
+            <div id="sidenav" style={bgTriangle}>
+                <span className="closebtn" onClick={closeNav}>&times;</span>
                 <a href="#">About</a>
                 <a href="#">Services</a>
                 <a href="#">Clients</a>
