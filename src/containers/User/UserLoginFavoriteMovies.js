9876ccf1753b917891d3f43ac4b86bfdb22a2865
Myriam <haasmyriam8@gmail.com>
2018-07-03 16:55:24 +0200

    Can select movies and save it the state for registrationMovieSelection

--- src/containers/User/UserLoginFavoriteMovies.js
+++ src/containers/User/UserLoginFavoriteMovies.js
@@ -1,7 +1,50 @@
 import React, {Component} from "react";
-import { Grid, Cell, TextField } from 'react-md';
+import { Grid, Cell } from 'react-md';
+import axios from 'axios';
 
 export default class UserLoginFavoriteMovies extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            favoriteMovies : []
+        };
+    }
+
+    selectMovie = (movieId, e) => {
+        var moviesId = this.state.favoriteMovies;
+
+        if(e.target.parentElement.classList.contains('active')) {
+            moviesId.splice(moviesId.indexOf(movieId, 1));
+            this.setState({favoriteMovies: moviesId});
+            e.target.parentElement.classList.remove('active');
+        } else {
+            moviesId.push(movieId);
+            this.setState({favoriteMovies: moviesId});
+            e.target.parentElement.classList.add('active');
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`${process.env.REACT_APP_API_URL}/movies/populars`)
+        .then((response) => {
+            let self = this;
+
+            const moviesList = response.data.map(function(item){
+                return(
+                    <Cell className="p-1 movies_gallery" key={item.id}>
+                        <i className="material-icons">check_circle</i>
+                        <img src={item.cover} alt={item.cover} onClick={(e) => self.selectMovie(item.id, e)}/>
+                    </Cell>
+                );
+            });
+
+            this.setState({moviesPopularList : moviesList});
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
 
     render() {
     
@@ -17,10 +60,7 @@ export default class UserLoginFavoriteMovies extends Component{
         
                     <Cell size={8} className="m-0">
                         <Grid className="p-0">
-                            <Cell className="p-1 movies_gallery">
-                                <i className="material-icons">check_circle</i>
-                                <img src="https://vignette.wikia.nocookie.net/batman/images/b/bb/TDKR_Batman_poster-1.jpg/revision/latest?cb=20120523032614" alt="Poster" className="likeMovie" data-movieId=""/>
-                            </Cell>
+                            {this.state.moviesPopularList}
                         </Grid>
                     </Cell>
                 </Grid>
