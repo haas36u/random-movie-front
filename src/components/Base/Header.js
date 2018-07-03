ce9e39f977f74d53fb5d97cc92bf53e9e0845cf8
Myriam <haasmyriam8@gmail.com>
2018-07-03 15:55:02 +0200

    Save in state user movieTitle

--- src/components/Base/Header.js
+++ src/components/Base/Header.js
@@ -5,6 +5,15 @@ import { Link } from 'react-router-dom';
 import Menu from './Menu';
 
 export default class Footer extends Component{
+
+    handleChangeMovieTitle = (value) => {
+        this.setState({movieTitle: value});
+    }
+
+    searchMovie = () => {
+        console.log(this.state.movieTitle)
+    }
+
     render() {
         return (
             <header>
@@ -13,8 +22,8 @@ export default class Footer extends Component{
                     <li><Link to="/movies">Les films</Link></li>
                 </ul>
                 <div className="flex">
-                    <TextField id="search" placeholder="Rechercher" className="search" type="search"/>
-                    <button type="submit"><i className="fas fa-search"></i></button>
+                    <TextField id="search" placeholder="Rechercher" className="search" type="search" onChange={this.handleChangeMovieTitle}/>
+                    <button onClick={this.searchMovie}><i className="fas fa-search"></i></button>
                 </div>
                 <nav>
                     <Menu />
