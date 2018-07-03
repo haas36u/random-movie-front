0fb76ca082955e59c822760ee0f79b449ff67a66
Myriam <haasmyriam8@gmail.com>
2018-07-03 21:41:35 +0200

    Can do some actions in movies (watch, wish and favorite)

--- src/components/Movie/MovieActions.js
+++ src/components/Movie/MovieActions.js
@@ -10,7 +10,7 @@ export default class MovieActions extends Component {
 
         axios({method: 'put', url: `${process.env.REACT_APP_API_URL}/movies/${this.props.movieId}/like`, headers: {"Authorization" : localStorage.getItem('token')} })
         .then((response) => {
-            let elementClassList = document.getElementById('favorite-' + this.props.movieId).classList;
+            let elementClassList = document.getElementById('js-favorite-' + this.props.movieId).classList;
             if(elementClassList.contains('active')) {
                 elementClassList.remove('active');
             }else{
@@ -24,12 +24,36 @@ export default class MovieActions extends Component {
 
     addToWatched = (e) => {
         e.stopPropagation();
-        console.log('addToWatched', this.props.movieId)
+        
+        axios({method: 'put', url: `${process.env.REACT_APP_API_URL}/movies/${this.props.movieId}/watch`, headers: {"Authorization" : localStorage.getItem('token')} })
+        .then((response) => {
+            let elementClassList = document.getElementById('js-watch-' + this.props.movieId).classList;
+            if(elementClassList.contains('active')) {
+                elementClassList.remove('active');
+            }else{
+                elementClassList.add('active');
+            }  
+        })
+        .catch(error => {
+            console.log(error)
+        });
     }
 
     addToWishList = (e) => {
         e.stopPropagation();
-       console.log('addToWishList', this.props.movieId)
+       
+        axios({method: 'put', url: `${process.env.REACT_APP_API_URL}/movies/${this.props.movieId}/wish`, headers: {"Authorization" : localStorage.getItem('token')} })
+        .then((response) => {
+            let elementClassList = document.getElementById('js-wish-' + this.props.movieId).classList;
+            if(elementClassList.contains('active')) {
+                elementClassList.remove('active');
+            }else{
+                elementClassList.add('active');
+            }  
+        })
+        .catch(error => {
+            console.log(error)
+        });
     }
 
     render() {
@@ -38,9 +62,9 @@ export default class MovieActions extends Component {
             if(isLogin()) {
                 return (
                     <span className="movie-actions-container">
-                        <Button icon onClick={(e) => this.addToFavorite(e)} tooltipLabel="Ajouter à vos favoris"><i className="fas fa-heart" id={'favorite-' + this.props.movieId}></i></Button>
-                        <Button icon onClick={(e) => this.addToWatched(e)}  tooltipLabel="Ajouter aux films déjà vus"><i className="fas fa-eye" id={'watched-' + this.props.movieId}></i></Button>
-                        <Button icon onClick={(e) => this.addToWishList(e)} tooltipLabel="Ajouter à votre liste de film à voir"><i className="material-icons" id={'wished-' + this.props.movieId}>playlist_add</i></Button>
+                        <Button icon onClick={(e) => this.addToFavorite(e)} tooltipLabel="Ajouter à vos favoris"><i className="fas fa-heart" id={'js-favorite-' + this.props.movieId}></i></Button>
+                        <Button icon onClick={(e) => this.addToWatched(e)}  tooltipLabel="Ajouter aux films déjà vus"><i className="fas fa-eye" id={'js-watch-' + this.props.movieId}></i></Button>
+                        <Button icon onClick={(e) => this.addToWishList(e)} tooltipLabel="Ajouter à votre liste de film à voir"><i className="material-icons" id={'js-wish-' + this.props.movieId}>playlist_add</i></Button>
                     </span>
                 )
             }
