b2e261bafde7a481a814d3f7c10bdc44506bef6c
Myriam <haasmyriam8@gmail.com>
2018-07-08 18:25:10 +0200

    Remove dotenv.js because already exist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './style/main.scss';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-require('dotenv').config();
 const store = configureStore()
 
 const jsx = (
