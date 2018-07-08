cd9af5a32abeadfff45e0afd64fdea0a430fcda8
Myriam <haasmyriam8@gmail.com>
2018-07-08 18:18:52 +0200

    Remove serviceworker import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/main.scss';
 import AppRouter from './routers/AppRouter';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 require('dotenv').config();
