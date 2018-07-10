9df7253cee379662fdde304b3810f79275bc47b2
Myriam <haasmyriam8@gmail.com>
2018-07-10 21:41:50 +0200

    Change the organization of social page

--- src/containers/Social/SocialIndex.js
+++ src/containers/Social/SocialIndex.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Avatar } from 'react-md';
+import { Link } from 'react-router-dom';
+import { Grid, Cell, Avatar } from 'react-md';
 
 import SocialItem from '../../components/Social/SocialItem';
 import SocialItemTableau from '../../components/Social/SocialItemTableau';
@@ -72,85 +73,42 @@ export default class SocialIndex extends Component {
         return (
             <div id="social">
                 <div className="socialContainer">
-                    <div className="social__item--background userFollow">
-                        <h2>Vous suivez...</h2>
-                        <div>
-                            <i className="fas fa-plus-circle"></i>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                            <div>
-                                <Avatar src={avatar} role="presentation"/>
-                                <p>François</p>
-                            </div>
-                        </div>
+                    <div className="socialContainer__items">
+                        <SocialItem actuality={actuality} />
+                        <SocialItem actuality={actualityLiked} />
+                        <SocialItem actuality={actualityComment} />
                     </div>
 
-                    <SocialItem actuality={actuality} />
-                    <SocialItem actuality={actualityLiked} />
-                    <SocialItem actuality={actualityComment} />
+                    <div className="userFollow">
+                        <h2>Vous suivez...</h2>
+                        <div>
+                            <div className="userFollow__user">
+                                <Avatar src={avatar} role="presentation"/>
+                                <p>François</p>
+                            </div>
+                            <div className="userFollow__user">
+                                <Avatar src={avatar} role="presentation"/>
+                                <p>François</p>
+                            </div>
+                            <div className="userFollow__user">
+                                <Avatar src={avatar} role="presentation"/>
+                                <p>François</p>
+                            </div>
+                            <div className="userFollow__user">
+                                <Avatar src={avatar} role="presentation"/>
+                                <p>François</p>
+                            </div>
+                            <div className="userFollow__user">
+                                <Avatar src={avatar} role="presentation"/>
+                                <p>François</p>
+                            </div>
+                            <Link to="/" className="userFollow__user">
+                                <i className="fas fa-plus-circle"></i>
+                                <p>Ajouter un ami</p>
+                            </Link>
+                            <Link to="/">Voir la liste complète</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
