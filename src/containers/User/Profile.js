64b2ea3e87e3f5de31c0c3995ed65f0f34a6ba4c
Myriam <haasmyriam8@gmail.com>
2018-07-11 14:51:12 +0200

    Add loader spinner and refactor setState

--- src/containers/User/Profile.js
+++ src/containers/User/Profile.js
@@ -28,7 +28,8 @@ export default class Profile extends Component {
             favortieMoviesType : {},
             favortieMoviesTypeLegend: [],
             noDataWatchedMovies : null,
-            noDataNotation : null
+            noDataNotation : null,
+            loader : <span className="spinner"><svg width="150px"  height="150px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-double-ring"><circle cx="50" cy="50" ng-attr-r="{{config.radius}}" ng-attr-stroke-width="{{config.width}}" ng-attr-stroke="{{config.c1}}" ng-attr-stroke-dasharray="{{config.dasharray}}" fill="none" stroke-linecap="round" r="40" stroke-width="4" stroke="#bd4030" stroke-dasharray="62.83185307179586 62.83185307179586" transform="rotate(328.301 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="3.3s" begin="0s" repeatCount="indefinite"></animateTransform></circle><circle cx="50" cy="50" ng-attr-r="{{config.radius2}}" ng-attr-stroke-width="{{config.width}}" ng-attr-stroke="{{config.c2}}" ng-attr-stroke-dasharray="{{config.dasharray2}}" ng-attr-stroke-dashoffset="{{config.dashoffset2}}" fill="none" stroke-linecap="round" r="35" stroke-width="4" stroke="#e0b83e" stroke-dasharray="54.97787143782138 54.97787143782138" stroke-dashoffset="54.97787143782138" transform="rotate(-328.301 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;-360 50 50" keyTimes="0;1" dur="2s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg> </span>
         };
     }
 
@@ -68,14 +69,10 @@ export default class Profile extends Component {
                 );
             });
 
-            if(fullUser.notations.length === 0) notationsList = <p>Vous n'avez noté aucun film</p>;
-            if(fullUser.comments.length === 0) commentsList = <p>Vous n'avez pas encore commenté de film</p>;
+            if (fullUser.notations.length === 0) notationsList = <p>Vous n'avez noté aucun film</p>;
+            if (fullUser.comments.length === 0) commentsList = <p>Vous n'avez pas encore commenté de film</p>;
 
-            this.setState({user: user});
-            this.setState({notationsList: notationsList});
-            this.setState({nbNotations: fullUser.notations.length});
-            this.setState({commentsList: commentsList});
-            this.setState({nbComments: fullUser.comments.length});
+            this.setState({user: user, notationsList: notationsList, nbNotations: fullUser.notations.length, commentsList: commentsList, nbComments: fullUser.comments.length, loader: null});
         })
         .catch(error => {
             console.log(error)
@@ -146,7 +143,6 @@ export default class Profile extends Component {
                     }
                 }
             };
-            this.setState({favortieMoviesType: favortieMoviesType});
 
             if(stats.length > 6) stats.splice(6, stats.length - 6);
 
@@ -158,7 +154,7 @@ export default class Profile extends Component {
                     </Cell>
                 )
             });
-            this.setState({favortieMoviesTypeLegend: favortieMoviesTypeLegend});
+            this.setState({favortieMoviesType: favortieMoviesType, favortieMoviesTypeLegend: favortieMoviesTypeLegend});
         });
     }
 
@@ -221,6 +217,7 @@ export default class Profile extends Component {
 
         return (
         <div id="user-profile">
+            {this.state.loader}
             <div className="user-profile__header background-trianglify" style={bgTriangle}>
                 <div className="container">
                     <a href="" className="btn right">Modifier</a>
