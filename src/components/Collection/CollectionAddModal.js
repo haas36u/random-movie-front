279a1eab874dbe104d7a8b8e6dcf316c68dfdfb5
Myriam <haasmyriam8@gmail.com>
2018-07-11 18:27:04 +0200

    Create modal add collection and create custom modal

--- src/components/Collection/CollectionAddModal.js
+++ src/components/Collection/CollectionAddModal.js
@@ -3,17 +3,21 @@ import { Grid, Cell, TextField, DialogContainer } from 'react-md';
 
 export default class CollectionAddModal extends Component {
 
-    hadnleChangeTitle = (value) => {
+    handleChangeTitle = (value) => {
         console.log(value)
     }
     
     render() {
-        const hideModal = () => {
-            this.setState({visible: false});
+        const hideModal = (e) => {
+            if(e.target === document.getElementById('collectionAddModal')) cancel();
+        }
+
+        const cancel = () => {
+            document.getElementById('collectionAddModal').style.display = 'none';
         }
 
         return (
-           <div className="modal" id="collectionAddModal">
+           <div className="modal" id="collectionAddModal" onClick={hideModal}>
                 <div className="collectionModal modal-content">
                     <h2>Créer une collection</h2>
                     <Grid className="vertically-centered">
@@ -35,7 +39,7 @@ export default class CollectionAddModal extends Component {
                     </Grid>
 
                     <div className="text-right">
-                        <div className="btn mr-1 cancel" onClick={hideModal}>Annuler</div>
+                        <div className="btn mr-1 cancel" onClick={cancel}>Annuler</div>
                         <div className="btn">Créer</div>
                     </div>
                 </div>
