9a957d6a704f3c6979dadc6f3dbeafed74982133
Myriam <haasmyriam8@gmail.com>
2018-07-13 23:55:33 +0200

    Add link to wikipedia for actors with regex to convert actor name

--- src/components/Actor/ActorCard.js
+++ src/components/Actor/ActorCard.js
@@ -5,9 +5,11 @@ import { Card, CardText } from 'react-md';
 export default class ActorCard extends Component {
 
     render() {
+
+        const link = this.props.character.actor.name.replace(/(?:_| |\b)(\w)/g, function($1){return $1.toUpperCase().replace(' ','_');});
     
         return (
-            <Link to={`/actors/${this.props.character.id}`}>
+            <a href={`https://fr.wikipedia.org/wiki/${link}`} target="_blank">
                 <Card className="actor_container cursor">
                     <img src={this.props.character.actor.profile} alt={this.props.character.actor.name} />
                     <CardText>
@@ -15,7 +17,7 @@ export default class ActorCard extends Component {
                         <p className="actor_role">{this.props.character.role}</p>
                     </CardText>
                 </Card>
-            </Link>
+            </a>
         );
     }
 }
