3f4529bfca70ed55974591594d368af4877bf49a
Vincent Monjaret <vincent.monjaret@modis.com>
2018-07-17 18:11:27 +0200

    Added lazy loading

--- src/containers/Social/SocialIndex.js
+++ src/containers/Social/SocialIndex.js
@@ -1,89 +1,149 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { Avatar } from 'react-md';
+import {Link} from 'react-router-dom';
+import {Avatar} from 'react-md';
 
 import SocialItem from '../../components/Social/SocialItem';
 
 export default class SocialIndex extends Component {
 
-    constructor(props){
-        axios.defaults.headers['Content-Type'] = 'application/json';
-        axios.defaults.headers['Accept'] = 'application/json';
-        super(props);
-        this.state = {
-            actualityList: [],
-            followedUsers : [],
-            loader : this.loader
-        };
+  constructor(props) {
+    axios.defaults.headers['Content-Type'] = 'application/json';
+    axios.defaults.headers['Accept'] = 'application/json';
+    super(props);
+    this.state = {
+      total: 0,
+      currentCount: 0,
+      offset: 3,
+      actualityList: [],
+      isFetching: false,
+      currentPage: 1,
+      followedUsers: [],
+      // loader : this.loader
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.loadOnScroll);
+    this.getTotalFeed();
+    this.getFollowedUsers();
+  }
+
+  getTotalFeed = () => {
+    axios({
+      method: 'get',
+      url: `${process.env.REACT_APP_API_URL}/feeds/count`,
+      headers: {"Authorization": localStorage.getItem('token'), 'Content-Type': 'application/json'},
+    })
+      .then((response) => {
+        this.setState({total: response.data});
+
+        this.getActuality()
+      });
+  }
+
+  loadOnScroll = () => {
+    if (this.state.currentCount === this.state.total) return;
+    let el = document.getElementById('content-end');
+    let rect = el.getBoundingClientRect();
+    let isAtEnd = (
+      // rect.top >= 0 &&
+      // rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+    if (isAtEnd) {
+      //User at the end of content. load more content
+      if (!this.state.isFetching) {
+
+        this.setState({isFetching: true});
+
+        //get content from server
+        if (this.state.currentCount !== this.state.total) {
+          this.getActuality()
+        }
+      }
     }
+  }
 
-    loader = <span className="spinner"><svg width="150px"  height="150px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="lds-double-ring"><circle cx="50" cy="50" ng-attr-r="{{config.radius}}" ng-attr-stroke="{{config.c1}}" ng-attr-stroke-dasharray="{{config.dasharray}}" fill="none" strokeLinecap="round" r="40" strokeWidth="4" stroke="#bd4030" strokeDasharray="62.83185307179586 62.83185307179586" transform="rotate(328.301 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="3.3s" begin="0s" repeatCount="indefinite"></animateTransform></circle><circle cx="50" cy="50" ng-attr-r="{{config.radius2}}" ng-attr-stroke="{{config.c2}}" fill="none" strokeLinecap="round" r="35" strokeWidth="4" stroke="#e0b83e" strokeDasharray="54.97787143782138 54.97787143782138" strokeDashoffset="54.97787143782138" transform="rotate(-328.301 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;-360 50 50" keyTimes="0;1" dur="2s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg> </span>;        
+  getActuality = () => {
+    axios({
+      method: 'get',
+      url: `${process.env.REACT_APP_API_URL}/feeds`,
+      headers: {"Authorization": localStorage.getItem('token'), 'Content-Type': 'application/json'},
+      params: {page: this.state.currentPage},
+    })
+      .then((response) => {
+        this.setState((prevState) => {
+          return {
+            isFetching: false,
+            currentCount: prevState.currentCount + response.data.length,
+            actualityList: [...prevState.actualityList, ...response.data],
+            currentPage: prevState.currentPage + 1
+          }
+        })
+      });
+  }
 
-    componentDidMount() {
-        this.getActuality();
-        this.getFollowedUsers();
-    }
+  getFollowedUsers = () => {
+    let avatar = require('../../images/avatar_default.jpg');
 
-    getActuality = () => {
-        axios({method: 'get', url : `${process.env.REACT_APP_API_URL}/feeds`, headers : {"Authorization" : localStorage.getItem('token'), 'Content-Type': 'application/json'}})
-        .then((response) => {
-            let actualityList = response.data.map(function(item, key){
-                return (
-                    <SocialItem actuality={item} key={key} />
-                )
-            });
+    axios({
+      method: 'get',
+      url: `${process.env.REACT_APP_API_URL}/users/follows`,
+      headers: {"Authorization": localStorage.getItem('token'), 'Content-Type': 'application/json'}
+    })
+      .then((response) => {
+        const users = response.data.slice(0, 4);
 
-            if (actualityList.length === 0) actualityList = <p>Aucune activité a été enregistrée de la part de vos abonnements</p>
-
-            this.setState({actualityList: actualityList, loader: null});
+        const followedUsers = users.map(function (user) {
+          return (
+            <Link to={`/profile/${user.id}`} className="userFollow__user" key={user.id}>
+              <Avatar src={avatar} role="presentation"/>
+              <p>{user.username}</p>
+            </Link>
+          )
         });
-    }
 
-    getFollowedUsers = () => {
-        let avatar = require('../../images/avatar_default.jpg');
+        this.setState({followedUsers: followedUsers});
+      });
+  }
 
-        axios({method: 'get', url : `${process.env.REACT_APP_API_URL}/users/follows`, headers : {"Authorization" : localStorage.getItem('token'), 'Content-Type': 'application/json'}})
-        .then((response) => {
-            const users = response.data.slice(0, 4);
+  render() {
 
-            const followedUsers = users.map(function(user){
-                return (
-                    <Link to={`/profile/${user.id}`} className="userFollow__user" key={user.id}>
-                        <Avatar src={avatar} role="presentation"/>
-                        <p>{user.username}</p>
-                    </Link>
-                )
-            });
+    return (
+      <div id="social">
+        {/*{this.state.loader}*/}
+        <div className="socialContainer">
+          <div className="socialContainer__items">
+            {
+              this.state.actualityList.length > 0 &&
+              this.state.actualityList.map((current, key) => {
+                return <SocialItem actuality={current} key={key}/>
+              })
+            }
+            {
+              (this.state.currentCount !== this.state.total) ?
+                <div id="content-end">
+                  Please wait. Loading...
+                </div> : null
+            }
+          </div>
 
-            this.setState({followedUsers: followedUsers});
-        });
-    }
-    
-    render() {
-        
-        return (
-            <div id="social">
-                {this.state.loader}
-                <div className="socialContainer">
-                    <div className="socialContainer__items">
-                        {this.state.actualityList}
-                    </div>
+          <div className="userFollow">
+            <h2>Vous suivez...</h2>
+            <div>
+              {this.state.followedUsers}
 
-                    <div className="userFollow">
-                        <h2>Vous suivez...</h2>
-                        <div>
-                            {this.state.followedUsers}
-                            
-                            <Link to="/social/search" className="userFollow__user">
-                                <i className="fas fa-plus-circle"></i>
-                                <p>Chercher un abonné</p>
-                            </Link>
-                            <Link to="/social/users">Voir la liste complète</Link>
-                        </div>
-                    </div>
-                </div>
+              <Link to="/social/search" className="userFollow__user">
+                <i className="fas fa-plus-circle"></i>
+                <p>Chercher un abonné</p>
+              </Link>
+              <Link to="/social/users">Voir la liste complète</Link>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
